3e834c329286503852995a97934fd930b9c3af57
Timothy <quiksylvir@gmail.com>
2024-09-24 21:00:51 -0400

    Add keys for all necessary Roman numerals

--- src/main/java/IntegerMap.java
+++ src/main/java/IntegerMap.java
@@ -6,5 +6,16 @@ public class IntegerMap {
     static {
         integerMap.put(1, "I");
         integerMap.put(4, "IV");
+        integerMap.put(5, "V");
+        integerMap.put(9, "IX");
+        integerMap.put(10, "X");
+        integerMap.put(40, "XL");
+        integerMap.put(50, "L");
+        integerMap.put(90, "XC");
+        integerMap.put(100, "C");
+        integerMap.put(400, "CD");
+        integerMap.put(500, "D");
+        integerMap.put(900, "DM");
+        integerMap.put(1000, "M");
     }
 }
